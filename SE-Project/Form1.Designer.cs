3bc93eb0ef6d8bf8dc43e61cdcf72c8675ff6897
james <james@WINDOWS>
2024-10-22 10:16:55 +0100

    Add project files.

---
+++ SE-Project/Form1.Designer.cs
@@ -0,0 +1,152 @@
+﻿namespace SE_Project
+{
+    partial class togetherculture_events
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(togetherculture_events));
+            this.textbox_dashboard = new System.Windows.Forms.TextBox();
+            this.searchevents_textbox = new System.Windows.Forms.TextBox();
+            this.title_textbox = new System.Windows.Forms.TextBox();
+            this.event_picture = new System.Windows.Forms.PictureBox();
+            this.book_textbox = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            ((System.ComponentModel.ISupportInitialize)(this.event_picture)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textbox_dashboard
+            // 
+            resources.ApplyResources(this.textbox_dashboard, "textbox_dashboard");
+            this.textbox_dashboard.Name = "textbox_dashboard";
+            // 
+            // searchevents_textbox
+            // 
+            resources.ApplyResources(this.searchevents_textbox, "searchevents_textbox");
+            this.searchevents_textbox.Name = "searchevents_textbox";
+            this.searchevents_textbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // title_textbox
+            // 
+            resources.ApplyResources(this.title_textbox, "title_textbox");
+            this.title_textbox.Name = "title_textbox";
+            this.title_textbox.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // event_picture
+            // 
+            resources.ApplyResources(this.event_picture, "event_picture");
+            this.event_picture.Name = "event_picture";
+            this.event_picture.TabStop = false;
+            // 
+            // book_textbox
+            // 
+            resources.ApplyResources(this.book_textbox, "book_textbox");
+            this.book_textbox.Name = "book_textbox";
+            this.book_textbox.UseVisualStyleBackColor = true;
+            this.book_textbox.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button1
+            // 
+            resources.ApplyResources(this.button1, "button1");
+            this.button1.Name = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox1
+            // 
+            resources.ApplyResources(this.pictureBox1, "pictureBox1");
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.TabStop = false;
+            // 
+            // textBox1
+            // 
+            resources.ApplyResources(this.textBox1, "textBox1");
+            this.textBox1.Name = "textBox1";
+            // 
+            // button2
+            // 
+            resources.ApplyResources(this.button2, "button2");
+            this.button2.Name = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox2
+            // 
+            resources.ApplyResources(this.pictureBox2, "pictureBox2");
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.TabStop = false;
+            // 
+            // textBox2
+            // 
+            resources.ApplyResources(this.textBox2, "textBox2");
+            this.textBox2.Name = "textBox2";
+            // 
+            // togetherculture_events
+            // 
+            resources.ApplyResources(this, "$this");
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.book_textbox);
+            this.Controls.Add(this.event_picture);
+            this.Controls.Add(this.title_textbox);
+            this.Controls.Add(this.searchevents_textbox);
+            this.Controls.Add(this.textbox_dashboard);
+            this.Name = "togetherculture_events";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.event_picture)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textbox_dashboard;
+        private System.Windows.Forms.TextBox searchevents_textbox;
+        private System.Windows.Forms.TextBox title_textbox;
+        private System.Windows.Forms.PictureBox event_picture;
+        private System.Windows.Forms.Button book_textbox;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.TextBox textBox2;
+    }
+}
+
